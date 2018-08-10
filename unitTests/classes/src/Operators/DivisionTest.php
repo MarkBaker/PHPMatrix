2c6c9c39d9db424c68a02ae789754010349d66a4
MarkBaker <mark@lange.demon.co.uk>
2018-08-10 19:20:26 +0100

    data provider for division operator

--- unitTests/classes/src/Operators/DivisionTest.php
+++ unitTests/classes/src/Operators/DivisionTest.php
@@ -61,23 +61,33 @@ class DivisionTest extends BaseTestAbstract
             ->result();
     }
 
-    public function testDivideScalar()
+    /**
+     * @dataProvider scalarDivisionProvider
+     */
+    public function testDivideScalar($original, $dividend, $expected)
     {
-        $expected = [[0.2, 0.4, 0.6], [0.8, 1.0, 1.2], [1.4, 1.6, 1.8]];
-        $original = $this->getTestGrid1();
         $matrix = new Matrix($original);
 
         $divisor = new Division($matrix);
 
-        $result = $divisor->execute(5)
+        $result = $divisor->execute($dividend)
             ->result();
 
         //    Must return an object of the correct type...
         $this->assertIsMatrixObject($result);
         //    ... containing the correct data
-        $this->assertMatrixValues($result, 3, 3, $expected);
+        $this->assertMatrixValues($result, count($expected), count($expected[0]), $expected);
         // Ensure that original matrix remains unchanged (Immutable object)
         $this->assertOriginalMatrixIsUnchanged($original, $matrix, 'Original Matrix has mutated');
+    }
+
+    public function scalarDivisionProvider()
+    {
+        return [
+            [$this->getTestGrid1(), 5, [[0.2, 0.4, 0.6], [0.8, 1.0, 1.2], [1.4, 1.6, 1.8]]],
+            [$this->getTestGrid1(), -12, [[-1 / 12, -1 / 6, -1 / 4], [-1 / 3, -5 / 12, -1 / 2], [-7 / 12, -2 / 3, -3 / 4]]],
+            [$this->getTestGrid1(), 0.25, [[4, 8, 12], [16, 20, 24], [28, 32, 36]]],
+        ];
     }
 
     /**
@@ -104,6 +114,8 @@ class DivisionTest extends BaseTestAbstract
     {
         return [
             [$this->getTestGrid1(), $this->getTestMatrix2()->toArray(), [[13 / 60, -1 / 30, 13 / 60], [5 / 12, 1 / 6, 5 / 12], [37 / 60, 11 / 30, 37 / 60]]],
+            [[[13, 26], [39, 13]], [[7, 4], [2, 3]], [[-1, 10], [7, -5]]],
+            [[[7, 4], [2, 3]], [[13, 26], [39, 13]], [[1 / 13, 2/ 13], [7 / 65, 1 / 65]]],
         ];
     }
 
